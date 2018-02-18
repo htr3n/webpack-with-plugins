0b755cc2dc60274514af418239706eb4428f2fc3
Huy Tran <hoang.huy.tran@gmail.com>
2018-02-18 13:53:44 +1100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]  
+};
